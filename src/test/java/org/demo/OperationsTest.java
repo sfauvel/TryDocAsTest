a1163516c4d6565eda1bdf7b2b7c90edb538ec99
sfauvel <sfauvel@gmail.com>
2021-08-13 00:09:31 +0200

    Add a new method and associated tests

--- src/test/java/org/demo/OperationsTest.java
+++ src/test/java/org/demo/OperationsTest.java
@@ -1,15 +1,34 @@
 package org.demo;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.sfvl.doctesting.junitextension.ApprovalsExtension;
+import org.sfvl.doctesting.utils.DocPath;
 import org.sfvl.doctesting.utils.DocWriter;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Paths;
 
 public class OperationsTest {
 
-    static DocWriter doc = new DocWriter();
     @RegisterExtension
-    static ApprovalsExtension extension = new ApprovalsExtension(doc);
+    static ApprovalsExtension doc = new ApprovalsExtension(new DocWriter());
+
+    @AfterAll
+    static public void writeFileToConvertToHtml() throws IOException {
+        final DocPath docPath = new DocPath(OperationsTest.class);
+
+        String content = String.join("\n",
+                doc.getDocWriter().defineDocPath(Paths.get(".")),
+                ":nofooter:",
+                "include::" + docPath.approved().fullname() + "[]");
+
+        try (FileWriter fileWriter = new FileWriter(docPath.page().path().toFile())) {
+            fileWriter.write(content);
+        }
+    }
 
     /**
      * In mathematics, a square is the result of multiplying a number by itself.
