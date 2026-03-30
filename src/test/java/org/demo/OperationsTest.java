a8216c8634fcfa12ad72d3f9fde0401bee74510e
Sébastien Fauvel <sfauvel@gmail.com>
2026-03-30 22:56:59 +0200

    Use doc-as-test library from DocAsTest repository

--- src/test/java/org/demo/OperationsTest.java
+++ src/test/java/org/demo/OperationsTest.java
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.RegisterExtension;
-import org.sfvl.doctesting.junitextension.ApprovalsExtension;
-import org.sfvl.doctesting.junitextension.HtmlPageExtension;
-import org.sfvl.doctesting.junitextension.SimpleApprovalsExtension;
+import com.github.docastest.doctesting.junitextension.ApprovalsExtension;
+import com.github.docastest.doctesting.junitextension.HtmlPageExtension;
+import com.github.docastest.doctesting.junitextension.SimpleApprovalsExtension;
 
 @ExtendWith(HtmlPageExtension.class)
 @DisplayName("Operations")
